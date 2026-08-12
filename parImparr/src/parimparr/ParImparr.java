f3d8f4ab1b712460ddde0d6ed954cf1d576db34b
CristelaMorales27 <cristela.morales23@itca.edu.sv>
2026-08-11 19:52:53 -0600

    Agregando solucion para encontrar nueros impares

--- parImparr/src/parimparr/ParImparr.java
+++ parImparr/src/parimparr/ParImparr.java
@@ -1,20 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
+
 package parimparr;
 
 /**
  *
- * @author MINEDUCYT
+ * @author Cristela Morales 
  */
+
+import java.util.Scanner;
 public class ParImparr {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("INGRESE UN NUMERO");
+        
+        int numero = teclado.nextInt();
+        
+        if (numero % 2 == 0){
+        System.out.println(numero + " es par");
+        }else { 
+            System.out.println(numero + " es impar");
+        }
+        
+        
     }
     
 }
